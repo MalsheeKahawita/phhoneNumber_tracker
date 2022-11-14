f587564a8971cc32df6f8301ddcd49fa1c89b5b6
malshee <maashihansana@icloud.com>
2022-11-14 20:20:42 +0530

    service provider added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import phonenumbers
 from test import number
-
 from phonenumbers import geocoder
-ch_number = phonenumbers.parse(number,"CH")
+
+ch_number = phonenumbers.parse(number, "CH")
 print(geocoder.description_for_number(ch_number, "en"))
 
-#hghhjjh
+from phonenumbers import carrier
+service_number = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number, "en"))
